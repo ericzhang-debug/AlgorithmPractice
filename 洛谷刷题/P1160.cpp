d5a2e60d554d56485b3735e4636113f062087061
Eric <15364519511@163.com>
2023-09-23 23:38:00 +0800

    9-23

---
+++ 洛谷刷题/P1160.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int N;
+	list<int> node;
+	node.push_back(1);
+	cin>>N;
+	for(int i=2;i<=N;i++){
+		int k,p;
+		cin>>k>>p;
+		for(auto temp:node){
+			if(k>=i){
+				
+				break;
+			}
+		}
+	}
+	
+	
+	return 0;
+}
+
